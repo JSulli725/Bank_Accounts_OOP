78d509134164c362a657f392ca9e588af2dc89b5
Jordan Sullivan <jsulli725@gmail.com>
2021-06-15 14:05:12 -0400

    updated to include User class

--- bank_accounts.py
+++ bank_accounts.py
@@ -29,13 +29,46 @@ class BankAccount:
         print(f"Total interest generated is: ${self.total_interest}")
         return self
 
+checking_account = BankAccount(0 , 0.005)
+savings_account = BankAccount(0 , 0.03)
 
-account1 = BankAccount(0 , 0.029)
-account2 = BankAccount(0 , 0.015)
+class User:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.account = BankAccount(0 , .002)
 
-print("\naccount1:")
-account1.deposit(25).deposit(40).deposit(33).withdraw(13).yield_interest().display_account_info()
+    # def make_deposit(self, amount):
+    #     self.account.deposit += amount
+    #     return self
 
+    # def make_withdrawal(self, amount):
+    #     self.account.withdraw -= amount
+    #     return self
 
-print("\naccount2:")
-account2.deposit(50).deposit(27).withdraw(12).withdraw(13).withdraw(8).withdraw(6).yield_interest().display_account_info()
+    def display_user_balance(self):
+        print(f"User: {self.name}, Balance: {self.account}")
+
+    def transfer_money(self, other_user , amount):
+        self.account.balance -= amount
+        other_user.account.balance += amount
+        self.amount_transferred = amount
+
+user1 = User("Bill" , "billybob@google.com")
+user2 = User("George" , "georgepwney@att.net")
+user3 = User("Sue" , "sue.heckleschmidt@abcfarms.com")
+
+print(f"User: {user1.name}")
+user1.account.deposit(20).deposit(100).deposit(50).withdraw(75)
+print(f"Balance: {user1.account.balance}")
+print()
+print(f"User: {user2.name}")
+user2.account.deposit(60).deposit(110).withdraw(12).withdraw(75)
+print(f"Balance: {user2.account.balance}")
+print()
+print(f"User: {user3.name}")
+user3.account.deposit(45).withdraw(12).withdraw(3).withdraw(9)
+print(f"Balance: {user3.account.balance}")
+print()
+user1.transfer_money(user3, 10)
+print(f"{user1.name} transferred ${user1.amount_transferred} to {user3.name}.\n{user1.name} now has ${user1.account.balance} \n{user3.name} now has ${user3.account.balance}")
